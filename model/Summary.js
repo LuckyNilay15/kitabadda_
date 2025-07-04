c8d72442a1947506bd04332efbc53f1c4f9fc803
Nilay Namit <109901909+LuckyNilay15@users.noreply.github.com>
2025-07-04 22:39:11 +0530

    Create Summary.js

---
+++ model/Summary.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const summarySchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+export default mongoose.model("Summary", summarySchema);
